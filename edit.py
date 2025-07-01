e7c7a1b3f1b3d9769bcbd7ed8f3114cfa88f48e1
Deiva-12 <deivanaii0405@gmail.com>
2025-07-01 13:20:50 +0530

    Complated all commits

---
+++ edit.py
@@ -0,0 +1,7 @@
+print("hello world")
+Name = input("Enter your name :")
+Age = input("My age :")
+num1 = int(input("Enter number :"))
+num2 = int(input("Enter number num2:"))
+sum_Result = num1+num2
+print("Result",sum_Result)
